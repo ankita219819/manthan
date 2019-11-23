ed668cd6fe696d2e49fcb03d581ca48cba1ec33c
ankita219819 <ankiii.gupta@gmail.com>
2019-11-23 17:56:20 +0530

    java commit

---
+++ Assessment1/src/manthan/assessment/bean/Bean.java
@@ -0,0 +1,44 @@
+package manthan.assessment.bean;
+
+public class Bean {
+	
+	private int uid;
+	private String username;
+	private String password;
+	private int age;
+	private String company;
+	
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public int getUid() {
+		return uid;
+	}
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+}
